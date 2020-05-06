44bbbbdc17c534ac62b7745c5a6f1de13ba6f845
Minyoung Kim <kru963@gmail.com>
2020-05-06 13:00:54 +0900

    중간고사 최종본.

--- src/jsp_hw/BookDAO.java
+++ src/jsp_hw/BookDAO.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
 import jsp_hw.DB;
 
 public class BookDAO {
@@ -28,26 +29,27 @@ public class BookDAO {
 			return list;
 		}
 	}
-	 public static List<Book> findByAuthor(String author) throws Exception {
-		 String sql = "SELECT b.*, d.id"+
-					"FROM book b LEFT JOIN category d ON b.id = d.id" +
-	                     "WHERE b.author LIKE ?";
-	        try (Connection connection = DB.getConnection("student1");
-	             PreparedStatement statement = connection.prepareStatement(sql)) {
-	            statement.setString(1, author + "%");
-	            try (ResultSet resultSet = statement.executeQuery()) {
-	                ArrayList<Book> list = new ArrayList<Book>();
-	                while (resultSet.next()) {
-	                   Book book = new Book();
-	                    student.setId(resultSet.getInt("id"));
-	                    student.setStudentNumber(resultSet.getString("studentNumber"));
-	                    student.setName(resultSet.getString("name"));
-	                    student.setDepartmentId(resultSet.getInt("departmentId"));
-	                    student.setYear(resultSet.getInt("year"));
-	                    student.setDepartmentName(resultSet.getString("departmentName"));
-	                    list.add(student);
-	                }
-	                return list;
-	            }
-	        }
+	public static List<Book> findByAuthor(String author) throws Exception {
+		String sql = "SELECT b.*, d.id"+
+				"FROM book b LEFT JOIN category d ON b.id = d.id" +
+				"WHERE b.author LIKE ?";
+		try (Connection connection = DB.getConnection("student1");
+				PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setString(1, author + "%");
+			try (ResultSet resultSet = statement.executeQuery()) {
+				ArrayList<Book> list = new ArrayList<Book>();
+				while (resultSet.next()) {
+					Book book = new Book();
+					book.setId(resultSet.getInt("id"));
+					book.setTitle(resultSet.getString("title"));
+					book.setAuthor(resultSet.getString("author"));
+					book.setCategoryId(resultSet.getString("categoryId"));
+					book.setPrice(resultSet.getInt("price"));
+					book.setPublisher(resultSet.getString("publisher"));
+					list.add(book);
+				}
+				return list;
+			}
+		}
+	}
 }
