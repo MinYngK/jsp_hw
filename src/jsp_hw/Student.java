75d8a369bfd9022ba8f6e4fc43263623094b75c6
Minyoung Kim <kru963@gmail.com>
2020-05-06 12:20:43 +0900

    중간고사 20분 점검

--- src/jsp_hw/Student.java
+++ src/jsp_hw/Student.java
@@ -1,58 +1,61 @@
 package jsp_hw;
 
+
 public class Student {
 
-	int id;
-	String studentNumber;
-	String name;
-	int departmentId;
-	int year;
+    int id;
+    String studentNumber;
+    String name;
+    int departmentId;
+    int year;
+    String departmentName;
 
-	public Student(int id, String studentNumber, String name, int departmentId, int year) {
-		this.id = id;
-		this.studentNumber = studentNumber;
-		this.name = name;
-		this.departmentId = departmentId;
-		this.year = year;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public String getStudentNumber() {
+        return studentNumber;
+    }
 
-	public String getStudentNumber() {
-		return studentNumber;
-	}
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
 
-	public void setStudentNumber(String studentNumber) {
-		this.studentNumber = studentNumber;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public int getDepartmentId() {
+        return departmentId;
+    }
 
-	public int getDepartmentId() {
-		return departmentId;
-	}
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 
-	public void setDepartmentId(int departmentId) {
-		this.departmentId = departmentId;
-	}
+    public int getYear() {
+        return year;
+    }
 
-	public int getYear() {
-		return year;
-	}
+    public void setYear(int year) {
+        this.year = year;
+    }
 
-	public void setYear(int year) {
-		this.year = year;
-	}
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
+
