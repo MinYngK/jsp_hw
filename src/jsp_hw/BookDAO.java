ab40483c1c938ee1af4853c8de4892a321593829
Minyoung Kim <kru963@gmail.com>
2020-05-06 12:40:41 +0900

    중간고사 40분 점검

--- src/jsp_hw/BookDAO.java
+++ src/jsp_hw/BookDAO.java
@@ -8,24 +8,46 @@ import jsp_hw.DB;
 
 public class BookDAO {
 	public static List<Book> findAll() throws Exception {
-		 String sql = "SELECT s.*, d.foreignkey  " +
-                 "FROM student s LEFT JOIN foreignkey d ON s.foreignkeyId = d.id";
-    try (Connection connection = DB.getConnection("student1");
-         PreparedStatement statement = connection.prepareStatement(sql);
-         ResultSet resultSet = statement.executeQuery()) {
-        ArrayList<Student> list = new ArrayList<Student>();
-        while (resultSet.next()) {
-            Student student = new Student();
-            student.setId(resultSet.getInt("id"));
-            student.setStudentNumber(resultSet.getString("studentNumber"));
-            student.setName(resultSet.getString("name"));
-            student.setDepartmentId(resultSet.getInt("departmentId"));
-            student.setYear(resultSet.getInt("year"));
-            student.setDepartmentName(resultSet.getString("departmentName"));
-            list.add(student);
-        }
-        return list;
-    }
-}
+		String sql = "SELECT b.*, d.id"+
+				"FROM book b LEFT JOIN category d ON b.id = d.id";
+		try (Connection connection = DB.getConnection("student1");
+				PreparedStatement statement = connection.prepareStatement(sql);
+				ResultSet resultSet = statement.executeQuery()) {
+
+			List<Book> list = new ArrayList<Book>();
+			while (resultSet.next()) {
+				Book book = new Book();
+				book.setId(resultSet.getInt("id"));
+				book.setTitle(resultSet.getString("title"));
+				book.setAuthor(resultSet.getString("author"));
+				book.setCategoryId(resultSet.getString("categoryId"));
+				book.setPrice(resultSet.getInt("price"));
+				book.setPublisher(resultSet.getString("publisher"));
+				list.add(book);
+			}
+			return list;
+		}
 	}
+	 public static List<Book> findByAuthor(String author) throws Exception {
+		 String sql = "SELECT b.*, d.id"+
+					"FROM book b LEFT JOIN category d ON b.id = d.id" +
+	                     "WHERE b.author LIKE ?";
+	        try (Connection connection = DB.getConnection("student1");
+	             PreparedStatement statement = connection.prepareStatement(sql)) {
+	            statement.setString(1, author + "%");
+	            try (ResultSet resultSet = statement.executeQuery()) {
+	                ArrayList<Book> list = new ArrayList<Book>();
+	                while (resultSet.next()) {
+	                   Book book = new Book();
+	                    student.setId(resultSet.getInt("id"));
+	                    student.setStudentNumber(resultSet.getString("studentNumber"));
+	                    student.setName(resultSet.getString("name"));
+	                    student.setDepartmentId(resultSet.getInt("departmentId"));
+	                    student.setYear(resultSet.getInt("year"));
+	                    student.setDepartmentName(resultSet.getString("departmentName"));
+	                    list.add(student);
+	                }
+	                return list;
+	            }
+	        }
 }
